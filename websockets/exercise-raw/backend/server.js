8a601204b85d4d78c02bb4c265685ba056f9fca9
Olzhas Kurikov <okur@hellofresh.com>
2022-03-08 17:17:48 +0100

    websocket: establish connection upgrade with websocket frontend and backend

--- websockets/exercise-raw/backend/server.js
+++ websockets/exercise-raw/backend/server.js
@@ -1,36 +1,48 @@
-import http from "http";
-import handler from "serve-handler";
-import nanobuffer from "nanobuffer";
-
-// these are helpers to help you deal with the binary data that websockets use
-import objToResponse from "./obj-to-response.js";
-import generateAcceptValue from "./generate-accept-value.js";
-import parseMessage from "./parse-message.js";
+import http from 'http';
+import handler from 'serve-handler';
+import nanobuffer from 'nanobuffer';
+import objToResponse from './obj-to-response.js';
+import generateAcceptValue from './generate-accept-value.js';
+import parseMessage from './parse-message.js';
 
 let connections = [];
 const msg = new nanobuffer(50);
 const getMsgs = () => Array.from(msg).reverse();
 
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: 'brian',
+    text: 'hi',
+    time: Date.now(),
 });
 
 // serve static assets
 const server = http.createServer((request, response) => {
-  return handler(request, response, {
-    public: "./frontend",
-  });
+    return handler(request, response, {
+        public: './frontend',
+    });
 });
 
-/*
- *
- * your code goes here
- *
- */
+server.on('upgrade', function (req, socket) {
+    if (req.headers['upgrade'] !== 'websocket') {
+        // we only care about websockets
+        socket.end('HTTP/1.1 400 Bad Request');
+        return;
+    }
+    const acceptKey = req.headers['sec-websocket-key'];
+    const acceptValue = generateAcceptValue(acceptKey);
+    const headers = [
+        'HTTP/1.1 101 Web Socket Protocol Handshake',
+        'Upgrade: WebSocket',
+        'Connection: Upgrade',
+        `Sec-WebSocket-Accept: ${acceptValue}`,
+        'Sec-WebSocket-Protocol: json',
+        '\r\n',
+    ];
+
+    socket.write(headers.join('\r\n'));
+
+    console.log('connection upgrade requested!');
+});
 
 const port = process.env.PORT || 8080;
-server.listen(port, () =>
-  console.log(`Server running at http://localhost:${port}`)
-);
+server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -1,29 +1,31 @@
-const chat = document.getElementById("chat");
-const msgs = document.getElementById("msgs");
-const presence = document.getElementById("presence-indicator");
+const chat = document.getElementById('chat');
+const msgs = document.getElementById('msgs');
+const presence = document.getElementById('presence-indicator');
 let allChat = [];
 
 // listen for events on the form
-chat.addEventListener("submit", function (e) {
-  e.preventDefault();
-  postNewMsg(chat.elements.user.value, chat.elements.text.value);
-  chat.elements.text.value = "";
+chat.addEventListener('submit', function (e) {
+    e.preventDefault();
+    postNewMsg(chat.elements.user.value, chat.elements.text.value);
+    chat.elements.text.value = '';
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+    // code goes here
 }
 
-/*
- *
- * your code goes here
- *
- */
+const ws = new WebSocket('ws://localhost:8080', ['json']);
+console.log(ws);
+
+ws.addEventListener('open', () => {
+    console.log('connected');
+    presence.innerText = 'connected';
+});
 
 function render() {
-  const html = allChat.map(({ user, text }) => template(user, text));
-  msgs.innerHTML = html.join("\n");
+    const html = allChat.map(({ user, text }) => template(user, text));
+    msgs.innerHTML = html.join('\n');
 }
 
 const template = (user, msg) =>
-  `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
+    `<li class="collection-item"><span class="badge">${user}</span>${msg}</li>`;
