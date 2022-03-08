bccf75ad924cfb0014e0e523729e8e29dd6ebfa2
Olzhas Kurikov <okur@hellofresh.com>
2022-03-08 15:04:13 +0100

    exercise 1:  simple polling - push and get with setTimeout

--- polling/exercise/backend/server.js
+++ polling/exercise/backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import nanobuffer from "nanobuffer";
-import morgan from "morgan";
+import express from 'express';
+import bodyParser from 'body-parser';
+import nanobuffer from 'nanobuffer';
+import morgan from 'morgan';
 
 // set up a limited array
 const msg = new nanobuffer(50);
@@ -9,25 +9,35 @@ const getMsgs = () => Array.from(msg).reverse();
 
 // feel free to take out, this just seeds the server with at least one message
 msg.push({
-  user: "brian",
-  text: "hi",
-  time: Date.now(),
+    user: 'brian',
+    text: 'hi',
+    time: Date.now(),
 });
 
 // get express ready to run
 const app = express();
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(express.static("frontend"));
+app.use(express.static('frontend'));
 
-app.get("/poll", function (req, res) {
-  // use getMsgs to get messages to send back
-  // write code here
+app.get('/poll', function (req, res) {
+    res.json({
+        msg: getMsgs(),
+    });
 });
 
-app.post("/poll", function (req, res) {
-  // add a new message to the server
-  // write code here
+app.post('/poll', function (req, res) {
+    const { user, text } = req.body;
+
+    msg.push({
+        user,
+        text,
+        time: Date.now(),
+    });
+
+    res.json({
+        status: ok,
+    });
 });
 
 // start the server
